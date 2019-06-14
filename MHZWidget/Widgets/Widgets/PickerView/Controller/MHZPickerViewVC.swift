e3dac5eb948787ce96e9ae9be787a39dcb1ec451
马浩哲 <411408809@qq.com>
2019-06-14 15:17:49 +0800

    更改文件路径

---
+++ MHZWidget/Widgets/Widgets/PickerView/Controller/MHZPickerViewVC.swift
@@ -0,0 +1,30 @@
+//
+//  MHZPickerViewVC.swift
+//  MHZWidget
+//
+//  Created by MaHaoZhe on 2019/6/14.
+//  Copyright © 2019 HachiTech. All rights reserved.
+//
+
+import UIKit
+
+class MHZPickerViewVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
